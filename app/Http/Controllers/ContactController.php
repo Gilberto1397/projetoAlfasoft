265b0a98b02c7cd15299866463ef1b6a05dadb67
Gilberto Azevedo Junior <gilberto.junior@dbseller.com.br>
2025-12-08 15:30:54 -0300

    implement contact creation form with validation and routing

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\ContactRequest;
 use App\Services\ReturnContactFormService;
 use Illuminate\View\View;
 
@@ -11,5 +12,10 @@ class ContactController extends Controller
     public function returnContactForm(): View
     {
         return (new ReturnContactFormService())->returnContactForm();
+    }
+
+    public function createContact(ContactRequest $request): View
+    {
+        dd($request->all());
     }
 }
--- resources/views/contact/contact-form.blade.php
+++ resources/views/contact/contact-form.blade.php
@@ -7,17 +7,20 @@
     <fieldset>
         <legend>Create a new contact</legend>
 
-        <form action="/" method="POST">
+        <form action="{{route('contact.create')}}" method="POST">
             @csrf
             <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
+            <input type="text" id="name" name="name" value="{{old('name')}}" required>
+            <span>{{ $errors->has('name') ? $errors->first('name') : '' }}</span>
 
             <label for="contact">Contact Number:</label>
-            <input type="text" id="contact" name="contact" required>
+            <input type="text" id="contact" name="contact" value="{{old('contact')}}" required>
+            <span>{{ $errors->has('contact') ? $errors->first('contact') : '' }}</span>
 
             <br><br>
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email" value="{{old('email')}}" required>
+            <span>{{ $errors->has('email') ? $errors->first('email') : '' }}</span>
 
             <br><br>
             <button type="submit">Save Contact</button>
