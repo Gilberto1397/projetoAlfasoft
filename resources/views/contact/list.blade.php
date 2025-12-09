dab3072d2f7ab2cf5c92376eaa01fd9dd9d17ce7
Gilberto Azevedo Junior <gilberto.junior@dbseller.com.br>
2025-12-09 20:47:36 -0300

    Adjusting list page front end

--- resources/views/contact/list.blade.php
+++ resources/views/contact/list.blade.php
@@ -3,36 +3,37 @@
 @section('title', 'List of contacts')
 
 @section('content')
+    <section style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 50%">
+        <span>{{$message ?? null}}</span>
 
-    <span>{{$message ?? null}}</span>
+        <h1>Contacts List</h1>
 
-    <h1>Contacts List</h1>
+        @if(empty($contacts))
+            <h1>You don't have any contacts yet.</h1>
+            <a style="text-decoration: none; font-size: 16px; background-color: #5858e7; color: white; padding: 17px; border-radius: 16px;"
+               href="{{route('contact.return-form')}}">Would you like to add one?</a>
+        @else
+            <a style="text-decoration: none; background-color: #5858e7; color: white; padding: 17px; border-radius: 16px;"
+               href="{{route('contact.return-form')}}">New Contact +</a>
+        @endif
 
-    @if(empty($contacts))
-        <h1>You don't have any contacts yet.</h1>
-        <a href="{{route('contact.return-form')}}">Would you like to add one?</a>
-    @endif
+        <br>
+        <br>
 
-    <a href="{{route('contact.return-form')}}">New Contact +</a>
+        @foreach($contacts as $contact)
+            <fieldset style="width: 50%; display: flex; flex-direction: column; align-items: center; row-gap: 10px; margin-bottom: 20px;">
+                <legend>Contact {{$contact->id}}</legend>
+                <span style="margin-bottom: 10px;">Name: <a href="{{route('contact.details', [$contact->id])}}">{{$contact->name}}</a></span>
 
-    <br>
-    <br>
-
-    @foreach($contacts as $contact)
-        <fieldset>
-            <legend>Contact {{$contact->id}}</legend>
-            <a href="{{route('contact.details', [$contact->id])}}">Name: {{$contact->name}}</a>
-            <br>
-            <br>
-            <a href="{{route('contact.return-form', [$contact->id])}}">Update contact</a>
-            <br>
-            <br>
-            <form method="post" action="{{route('contact.delete', [$contact->id])}}">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Delete contact">
-            </form>
-        </fieldset>
-    @endforeach
-
+                <div style="width: 100%; display: flex; column-gap: 30px; justify-content: center; align-items: center;">
+                    <a style="text-decoration: none; font-size: 16px; background-color: #5858e7; color: white; padding: 17px; border-radius: 16px;" href="{{route('contact.return-form', [$contact->id])}}">Update contact</a>
+                    <form method="post" action="{{route('contact.delete', [$contact->id])}}">
+                        @csrf
+                        @method('DELETE')
+                        <input style="text-decoration: none; font-size: 16px; background-color: #e71313; color: white; padding: 17px; border-radius: 16px;" type="submit" value="Delete contact">
+                    </form>
+                </div>
+            </fieldset>
+        @endforeach
+    </section>
 @endsection()
