ccbd10c27926c648e7912e924370d59cde121a93
Gilberto Azevedo Junior <gilberto.junior@dbseller.com.br>
2025-12-10 09:11:42 -0300

    Adjusting contact details form page front end

--- resources/views/contact/contact-details.blade.php
+++ resources/views/contact/contact-details.blade.php
@@ -3,18 +3,20 @@
 @section('title', 'Contact Details')
 
 @section('content')
-    <fieldset>
-        <legend>Contact {{$contact->id}}</legend>
-        <p>Name: {{$contact->name}}</p>
-        <p>Contact: {{$contact->contact}}</p>
-        <p>Email: {{$contact->email}}</p>
+    <section style="display: flex; flex-direction: column; align-items: center; justify-content: center; width: 50%">
+        <fieldset style="width: 100%; display: flex; flex-direction: column; align-items: center;">
+            <legend>Contact {{$contact->id}}</legend>
+            <p>Name: {{$contact->name}}</p>
+            <p>Contact: {{$contact->contact}}</p>
+            <p>Email: {{$contact->email}}</p>
 
-        <br>
-        <br>
+            <div style="width: 40%;display: flex;justify-content: center;column-gap: 15px;">
+                <button style="text-decoration: none; font-size: 16px; background-color: #5858e7; color: white; padding: 17px; border-radius: 16px;"
+                        type="submit">{{empty($contact->id) ? 'Save Contact' : 'Update Contact'}}</button>
 
-        <a href="{{route('contact.return-form', [$contact->id])}}">Update contact</a>
-        <br>
-        <br>
-        <a href="{{route('contact.getall')}}">Go back</a>
-    </fieldset>
+                <a style="text-decoration: none; font-size: 16px; background-color: #e71313; color: white; padding: 17px; border-radius: 16px;"
+                   href="{{route('contact.getall')}}"><-- Go back</a>
+            </div>
+        </fieldset>
+    </section>
 @endsection()
